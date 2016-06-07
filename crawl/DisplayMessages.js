51486b42bb5074aadd52f9f696d9cfebc6065441
dmitriyprylutskyi <dprily@gmail.com>
2016-06-07 21:23:39 +0300

    Change on scrollTop

--- crawl/DisplayMessages.js
+++ crawl/DisplayMessages.js
@@ -4,39 +4,63 @@ var movingtext;
     var Messages = (function () {
         function Messages(options) {
             this.interval = 200000;
+            this.position = 0;
             this.speed = 1;
             for (var str in options)
                 this[str] = options[str];
             this.$el = $(options.selector);
+            this.height = this.$el.height();
             this.start();
+            this.loadData();
         }
         Messages.sendError = function (message) {
             $.post("crawl/log.php", message);
         };
+        Messages.prototype.onScrollEnd = function () {
+            var _this = this;
+            this.render();
+            this.stop();
+            this.position = 0;
+            setTimeout(function () {
+                _this.start();
+                _this.loadData();
+            }, 5000);
+        };
+        Messages.prototype.scroll = function () {
+            var _this = this;
+            if (this.isRuning)
+                requestAnimationFrame(function () { _this.scroll(); });
+            this.$el.scrollTop(this.position += this.speed);
+            var w = this.$el.scrollTop();
+            if (this.prev == w)
+                this.onScrollEnd();
+            this.prev = w;
+        };
         Messages.prototype.start = function () {
-            var that = this;
-            that.loadData();
-            setInterval(function () {
-                that.loadData();
-            }, this.interval);
+            this.isRuning = true;
         };
         Messages.prototype.loadData = function () {
             var _this = this;
-            var that = this;
             $.get(this.url, function (result) {
                 _this.messages = result;
-                that.render();
+                if (!_this.messages) {
+                    movingtext.Messages.sendError("this messages null");
+                    return;
+                }
+                _this.render();
+                _this.scroll();
             });
         };
+        Messages.prototype.stop = function () {
+            this.isRuning = false;
+        };
         Messages.prototype.render = function () {
-            var mov = $("<marquee behavior='scroll' direction='up' scrollamount='" + this.speed + "' height='560px'>");
-            if (!this.messages) {
-                movingtext.Messages.sendError("this messages null");
-                return;
-            }
+            var mov = $('<div>');
+            $('<p>').css('height', '560px').appendTo(mov);
             this.messages.forEach(function (item) {
                 $('<p>').html(item).appendTo(mov);
             });
+            $('<p>').css('height', '560px').appendTo(mov);
             this.$el.empty();
             this.$el.append(mov);
         };
@@ -48,7 +72,7 @@ var MTROptions = {
     selector: "#message-template",
     url: "crawl/gettext.php",
     interval: 25000,
-    speed: 3
+    speed: 1
 };
 var movingText = new movingtext.Messages(MTROptions);
 //# sourceMappingURL=DisplayMessages.js.map
--- crawl/DisplayMessages.ts
+++ crawl/DisplayMessages.ts
@@ -2,59 +2,91 @@
 
 
 module movingtext {
-     export class Messages{
+    export class Messages{
         private interval:number=200000;
+        private height:number;
         $el:JQuery;
         url:string;
         messages:Array<string>;
+        private position:number =0;
         private speed:number=1;
 
+        isRuning:boolean;
+
         static sendError (message) {
-         $.post ("crawl/log.php", message);
+            $.post ("crawl/log.php", message);
         }
 
         constructor(options:any){
             for(var str in options)this[str]=options[str];
             this.$el=$(options.selector);
+            this.height = this.$el.height();
             this.start();
+            this.loadData();
+        }
+
+
+        private onScrollEnd():void{
+            this.render();
+            this.stop();
+            this.position = 0;
+            setTimeout( () => {
+                this.start();
+                this.loadData();
+            }, 5000);
+         }
+
+        private prev:number;
+
+        scroll():void{
+            if(this.isRuning)requestAnimationFrame(()=>{  this.scroll(); });
+            this.$el.scrollTop(this.position+=this.speed);
+            var w = this.$el.scrollTop();
+            if(this.prev ==w)this.onScrollEnd();
+            this.prev = w;
         }
 
         start():void {
-            var that = this;
-            that.loadData();
-            setInterval(()=>{
-                that.loadData();
-            },this.interval);
+            this.isRuning = true;
         }
 
         loadData():void{
-            var that = this;
             $.get(this.url, (result:any [] ) =>{
                 this.messages = result;
-                that.render();
+                if (!this.messages) {
+                    movingtext.Messages.sendError ("this messages null");
+                    return;
+                }
+                this.render();
+                this.scroll();
             });
         }
 
+        stop(){
+            this.isRuning = false;
+        }
+
         private render(){
-            var mov =  $("<marquee behavior='scroll' direction='up' scrollamount='" + this.speed + "' height='560px'>");
-            if (!this.messages) {
-                movingtext.Messages.sendError ("this messages null");
-                return;
-            }
+            var mov =  $('<div>');
+            $('<p>').css('height', '560px').appendTo(mov);
+
             this.messages.forEach( function (item ) {
                 $('<p>').html(item).appendTo(mov);
             });
+
+            $('<p>').css('height', '560px').appendTo(mov);
             this.$el.empty();
             this.$el.append(mov);
         }
+
     }
 }
+
 var MTROptions={
     selector:"#message-template",
     url:"crawl/gettext.php",
     interval:25000,
-    speed:3
+    speed:1
 }
 
 var movingText = new movingtext.Messages(MTROptions);
-
