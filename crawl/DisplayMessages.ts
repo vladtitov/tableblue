f98fd0569f2780c0dafae662ed646dfe9cda59ad
dmitriyprylutskyi <dprily@gmail.com>
2016-06-07 12:09:31 +0300

    Update DispayMessages
    make text scroll speed slow, request server every 25 second and update
    text on screen

--- crawl/DisplayMessages.ts
+++ crawl/DisplayMessages.ts
@@ -4,69 +4,41 @@
 module movingtext {
      export class Messages{
         private interval:number=200000;
-        private width:number;
-        private isFirstTime:boolean=true;
         $el:JQuery;
         url:string;
-        requestParams:any;
         messages:Array<string>;
-        private position:number =0;
         private speed:number=1;
-         
-         sendError (message) {
-             $.post ("crawl/log.php", message);
-         }
 
-        isRuning:boolean
+        static sendError (message) {
+         $.post ("crawl/log.php", message);
+        }
 
         constructor(options:any){
             for(var str in options)this[str]=options[str];
             this.$el=$(options.selector);
             this.start();
-            this.loadData();
         }
 
-
-        private onScrollEnd():void{
-            this.render();
-            this.stop();
-            this.position = 0;
-            setTimeout(()=>{
-                this.start();
-                this.scroll();
-            },5000);
-        }
-
-        private prev:number;
-
-        scroll():void{
-            if(this.isRuning)requestAnimationFrame(()=>{  this.scroll(); });
-        }
-         
         start():void {
-            this.isRuning = true
+            var that = this;
+            that.loadData();
+            setInterval(()=>{
+                that.loadData();
+            },this.interval);
         }
 
         loadData():void{
+            var that = this;
             $.get(this.url, (result:any [] ) =>{
                 this.messages = result;
-                console.log(this.messages);
-                if (this.isFirstTime) {
-                    this.render();
-                    this.isFirstTime = false;
-                    this.scroll();
-                }
+                that.render();
             });
         }
 
-        stop(){
-            this.isRuning = false;
-        }
-
         private render(){
-            var mov =  $('<marquee behavior="scroll" direction="up" height="655px">');
+            var mov =  $("<marquee behavior='scroll' direction='up' scrollamount='" + this.speed + "' height='560px'>");
             if (!this.messages) {
-                this.sendError ("this messages null");
+                movingtext.Messages.sendError ("this messages null");
                 return;
             }
             this.messages.forEach( function (item ) {
@@ -81,7 +53,7 @@ var MTROptions={
     selector:"#message-template",
     url:"crawl/gettext.php",
     interval:25000,
-    speed:1
+    speed:3
 }
 
 var movingText = new movingtext.Messages(MTROptions);
